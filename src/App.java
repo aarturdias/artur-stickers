5d9af1c5efe7f1ba5e54c3db15598f244098956a
Artur Dias <aartur.dias@hotmail.com>
2023-05-08 01:01:17 -0300

    criando classe para gerar figurinhas

--- src/App.java
+++ src/App.java
@@ -1,11 +1,14 @@
-import java.io.IOException;
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
+import java.util.Map;
 
 public class App {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
 
         String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
         URI address = URI.create(url);
@@ -15,6 +18,20 @@ public class App {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
         String body = response.body();
 
-        System.out.println(body);
+        var parser = new JsonParser();
+        List<Map<String, String>> MoviesList = parser.parse(body);
+
+        var generator = new StickerGenerator();
+        for (Map<String, String> film : MoviesList) {
+
+            String imageUrl = film.get("image");
+            String title = film.get("title");
+
+            InputStream inputStream = new URL(imageUrl).openStream();
+
+            String fileName = "saida/" + title + ".png";
+            generator.create(inputStream, fileName);
+
+        }
     }
 }
